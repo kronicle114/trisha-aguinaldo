9f3a2e6887bf7af467e011cbb2332aa839623dc4
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-02-08 17:01:26 -0500

    a11y compliant

--- src/components/App.js
+++ src/components/App.js
@@ -30,32 +30,32 @@ class App extends Component {
         <Route exact path="/blog" component={Blog} />
         <Route exact path="/contact" component={Contact} />
   
-        <footer className="site-footer">
+        <footer className="site-footer" role="contentinfo">
           <div className="wrapper">
-            <ul role="menu" className="social-media-list" aria-labelledby="social-media-list">
+            <ul className="social-media-list" aria-label="Footer contact list">
               {/* <li className="footer-items">
                 <a href="http://www.trisha-aguinaldo.com/">
                   © 2019 Trisha Aguinaldo
                 </a>
               </li> */}
-              <li className="footer-items" role="menuitem">
+              <li className="footer-items" aria-label="contact list item">
                 <a href="https://twitter.com/kronicle1141" target="_blank" rel="noopener noreferrer">
                   <img className="footer-logo" src={twitter}alt="twitter logo"></img>
                 </a>
               </li>
-              <li className="footer-items" role="menuitem">
+              <li className="footer-items" aria-label="contact list item">
                 <a href="https://www.linkedin.com/in/paguinaldo/" target="_blank" rel="noopener noreferrer">
                   <img className="footer-logo" src={linkedin}alt="linkedin logo"></img>
                 </a>
               </li>
-              <li className="footer-items" role="menuitem">
+              <li className="footer-items" aria-label="contact list item">
                 <a href="https://github.com/kronicle114" target="_blank" rel="noopener noreferrer">
                   <img className="footer-logo" src={github}alt="github logo"></img>
                 </a>
               </li>
-              <li className="footer-items" role="menuitem">
+              <li className="footer-items" aria-label="contact list item">
                 <a href="mailto:trisha.aguinaldo@gmail.com">
-                  <img className="footer-logo" src={email}alt=""></img>
+                  <img className="footer-logo" src={email}alt="email logo"></img>
                 </a>
               </li>
               {/* <li className="footer-items">
