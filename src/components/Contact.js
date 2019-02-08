e61793588715085cda10e361b2411f25eb3b682f
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-02-08 15:35:00 -0500

    updated resume

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from '../assets/TA Web Dev Resume.pdf';
+import resume from '../assets/Resume.pdf';
 import map from '../assets/map.png';
 
 export default function Contact(props) {
