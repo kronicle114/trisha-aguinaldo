8b0afe89dd4af348bfa3044b05ce9fae6ecbe988
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-03-04 18:35:59 -0500

    added icons to contact, deleted telephono #

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/blog.css';
 
 export default function Blog(props) {
   return(
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import resume from '../assets/Resume.pdf';
 import map from '../assets/map.png';
+import twitter from '../assets/twitter.svg';
+import github from '../assets/github-icon.svg';
+import linkedin from '../assets/linkedin-icon.svg';
+import email from '../assets/gmail.svg';
 import '../styles/contact.css';
 
 export default function Contact(props) {
@@ -9,26 +13,35 @@ export default function Contact(props) {
       <h2>Contact</h2>
       <div className="contact-text">
       <p>
-        <strong>Patricia "Trisha" Aguinaldo</strong> currently resides in Milton, MA. Feel free to contact her for any questions. Office hours are from 9-5 EST. She also volunteers at Girls Who Code Boston and regularly attends tech meetups in Boston.
+        <strong>Patricia "Trisha" Aguinaldo</strong> currently resides in Milton, MA. Feel free to contact her for any questions. Office hours are from 9-5 EST. She regularly attends tech meetups in Boston and volunteers at Girls Who Code & Code for Boston.
       </p>
       <p className="resume">Download her resume <a href={resume}>here</a>.</p>
       </div>
       <div className="contacts-wrapper">
         <ul className="contact-list">
           <li className="contact-items">
-            (857) 231-3620
+            <a href="mailto:trisha.aguinaldo@gmail.com">
+              <img className="contact-icon" src={email}alt="email icon"/>
+              trisha.aguinaldo.gmail.com
+            </a>
           </li>
           <li className="contact-items">
-            trisha.aguinaldo.gmail.com
+            <a href="https://www.linkedin.com/in/paguinaldo/" target="_blank" rel="noopener noreferrer">
+            <img className="contact-icon" src={linkedin}alt="linkedin icon"/>
+              LinkedIn
+            </a>
           </li>
           <li className="contact-items">
-            <a href="https://www.linkedin.com/in/paguinaldo/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a href="https://github.com/kronicle114" target="_blank" rel="noopener noreferrer">
+              <img className="contact-icon" src={github}alt="github icon"/>
+              Github
+            </a>
           </li>
           <li className="contact-items">
-            <a href="https://github.com/kronicle114" target="_blank" rel="noopener noreferrer">Github</a>
-          </li>
-          <li className="contact-items">
-            <a href="https://twitter.com/kronicle1141" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <a href="https://twitter.com/kronicle1141" target="_blank" rel="noopener noreferrer">
+              <img className="contact-icon" src={twitter}alt="twitter icon"/>
+              Twitter
+            </a>
           </li>
         </ul>
         <img className="map" src={map} alt="Milton, MA static map"/>
