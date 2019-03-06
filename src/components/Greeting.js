9cd3d167a8a3b28f51a48bcc5f3f6b5ad4968478
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-03-06 11:49:43 -0500

    animation

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,9 +4,27 @@ import '../styles/greeting.css';
 export default function Greeting() {
   return(
     <div className="greeting">
-      <p className="blurb">
+      {/* <p className="blurb">
         Hi! I'm Trisha, a full stack developer with a background in healthcare and education.
-      </p>
+      </p> */}
+
+      <div className="animate color-delay">
+        <span>H</span>
+        <span>e</span>
+        <span>l</span>
+        <span>l</span>
+        <span>o</span>
+        <span>&nbsp;</span>
+        <span>W</span>
+        <span>o</span>
+        <span>r</span>
+        <span>l</span>
+        <span>d</span>
+        <span>!</span>
+        {/* <span>l</span>
+        <span>d</span>
+        <span>o</span> */}
+      </div> 
     </div>
   )
 }
