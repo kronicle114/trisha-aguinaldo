0c969954470979cab13856313f1c5fb8a24b188b
Trisha Aguinaldo <trisha.aguinaldo@gmail.com>
2019-03-06 12:21:46 -0500

    centering...

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,27 +4,29 @@ import '../styles/greeting.css';
 export default function Greeting() {
   return(
     <div className="greeting">
-      <div className="animate color-delay">
-        <span>H</span>
-        <span>e</span>
-        <span>l</span>
-        <span>l</span>
-        <span>o</span>
-        <span>&nbsp;</span>
-        <span>W</span>
-        <span>o</span>
-        <span>r</span>
-        <span>l</span>
-        <span>d</span>
-        <span>!</span>
-        {/* <span>l</span>
-        <span>d</span>
-        <span>o</span> */}
-      </div> 
-      <p className="blurb">
-        I'm <span className="highlight">Patricia Aguinaldo</span>, a full stack developer with a background in healthcare and education.
-      </p>
-      <a href="#about">View My Work</a>
+      <div className="greeting-contents">
+        <div className="animate color-delay">
+          <span>h</span>
+          <span>e</span>
+          <span>l</span>
+          <span>l</span>
+          <span>o</span>
+          <span>&nbsp;</span>
+          <span>w</span>
+          <span>o</span>
+          <span>r</span>
+          <span>l</span>
+          <span>d</span>
+          <span>.</span>
+          {/* <span>l</span>
+          <span>d</span>
+          <span>o</span> */}
+        </div> 
+        <p className="blurb">
+          I'm <span className="highlight">Patricia Aguinaldo</span>, a full stack developer with a background in healthcare and education.
+        </p>
+        <a href="#about">View My Work</a>
+      </div>
     </div>
   )
 }
