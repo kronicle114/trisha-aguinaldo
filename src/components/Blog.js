f135c76bc20f77b545d30157c630e8842365b2ff
Trisha Aguinaldo <trishaaguinaldo@Trishas-MacBook-Pro-2.local>
2021-03-14 10:35:52 -0400

    updates

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -16,6 +16,10 @@ export default function Blog(props) {
 
     <div className="blog-contents">
       <ul className="article-list">
+          <li className="articles">
+            <h3><a href="https://www.reggora.com/meet-the-team-trisha-aguinaldo/">Meet the Team: Trisha Aguinaldo</a></h3>
+            <p>February 5, 2021</p>
+          </li>
         <li className="articles">
           <h3><a href="https://dev.to/kronicle114/bash-profile-shortcuts-and-terminal-customizations-2lmj">Bash profile shortcuts and terminal customizations</a></h3>
           <p>Dec 23, 2019</p>
@@ -24,15 +28,15 @@ export default function Blog(props) {
           <h3><a href="https://dev.to/kronicle114/updating-vim-macos-797">Updating Vim (MacOS)</a></h3>
           <p>Jul 6, 2019</p>
         </li>
-	<li className="articles">
+	      {/* <li className="articles">
           <h3><a href="https://medium.com/@trishaaguinaldo/hide-secret-info-via-env-34acf446e32">Hide Secret Info via .env</a></h3>
           <p>Jul 4, 2019</p>
-        </li>
-	<li className="articles">
+        </li> */}
+	      <li className="articles">
           <h3><a href="https://medium.com/@trishaaguinaldo/how-did-i-spend-my-first-week-of-bootcamp-8539b129ce94">How I spent my first week of bootcamp</a></h3>
           <p>Nov 3, 2018</p>
         </li>
-<li className="articles">
+        <li className="articles">
           <h3><a href="https://www.linkedin.com/pulse/create-custom-domain-your-heroku-app-patricia-aguinaldo/">Create a Custom Domain for your Heroku App</a></h3>
           <p>January 22, 2019</p>
         </li>
